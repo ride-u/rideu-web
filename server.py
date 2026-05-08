660dadabbe5d5e922173671c847b3f0e31e87479
Ride-U <incoguatetra@gmail.com>
2026-05-07 19:26:13 -0600

    Add HEAD route

--- server.py
+++ server.py
@@ -511,6 +511,9 @@ async def generate_pin(request: Request):
 
     return {"ok": True, "pin": pin}
 
+@app.head("/")
+def head_home():
+    return
 
 @app.get("/students")
 def students():
@@ -544,3 +547,4 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("server:app", host="0.0.0.0", port=port)
+
